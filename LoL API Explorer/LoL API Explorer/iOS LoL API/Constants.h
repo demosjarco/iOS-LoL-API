fc9b87a67e798d781001633c7461a6145b3ac51d
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2015-01-20 13:43:31 -0700

    Added a constants file for enumerations

---
+++ LoL API Explorer/LoL API Explorer/iOS LoL API/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  LoL API Explorer
+//
+//  Created by Victor Ilisei on 1/20/15.
+//  Copyright (c) 2015 Tech Genius. All rights reserved.
+//
+
+#ifndef LoL_API_Explorer_Constants_h
+#define LoL_API_Explorer_Constants_h
+
+#endif
