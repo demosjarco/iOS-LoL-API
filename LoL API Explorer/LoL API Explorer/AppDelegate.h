f8ee68af5aee4c02ba2b7673ed6d73f4354b57ed
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2015-01-20 07:26:58 -0700

    Created project

---
+++ LoL API Explorer/LoL API Explorer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoL API Explorer
+//
+//  Created by Victor Ilisei on 1/20/15.
+//  Copyright (c) 2015 Tech Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoL API Explorer/LoL API ExplorerTests/LoL_API_ExplorerTests.m
@@ -0,0 +1,40 @@
+//
+//  LoL_API_ExplorerTests.m
+//  LoL API ExplorerTests
+//
+//  Created by Victor Ilisei on 1/20/15.
+//  Copyright (c) 2015 Tech Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LoL_API_ExplorerTests : XCTestCase
+
+@end
+
+@implementation LoL_API_ExplorerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
