8a1b1c2efb2d3a8abd49ca34546dbca456acb0be
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2015-01-20 12:25:14 -0700

    Configured local store

--- LoL API Explorer/LoL API Explorer/iOS LoL API/ILA_Setup.m
+++ LoL API Explorer/LoL API Explorer/iOS LoL API/ILA_Setup.m
@@ -10,6 +10,40 @@
 
 @implementation ILA_Setup
 
+#pragma mark - Private methods
+
++ (NSDictionary *)readKeyAndRegion {
+    @autoreleasepool {
+        NSString *filePath = [[[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"API DB"] stringByAppendingPathComponent:@"main"] stringByAppendingPathExtension:@"plist"];
+        return [NSDictionary dictionaryWithContentsOfFile:filePath];
+    }
+}
+
++ (BOOL)writeToLocalStore:(NSDictionary *)item inFolder:(NSString *)folderName withFilename:(NSString *)fileName {
+    @autoreleasepool {
+        NSString *filePath = [[[[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"API DB"] stringByAppendingPathComponent:folderName] stringByAppendingPathComponent:fileName] stringByAppendingPathExtension:@"plist"];
+        
+        @autoreleasepool {
+            NSFileManager *filemgr = [NSFileManager defaultManager];
+            if (![filemgr fileExistsAtPath:[filePath stringByDeletingLastPathComponent]]) {
+                [filemgr createDirectoryAtPath:[filePath stringByDeletingLastPathComponent] withIntermediateDirectories:NO attributes:nil error:nil];
+            }
+        }
+        
+        return [item writeToFile:filePath atomically:YES];
+    }
+}
+
++ (NSDictionary *)getItemFromLocalStoreForFilename:(NSString *)fileName inFolder:(NSString *)folderName {
+    @autoreleasepool {
+        NSString *filePath = [[[[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"API DB"] stringByAppendingPathComponent:folderName] stringByAppendingPathComponent:fileName] stringByAppendingPathExtension:@"plist"];
+        
+        return [NSDictionary dictionaryWithContentsOfFile:filePath];
+    }
+}
+
+#pragma mark - Public methods
+
 /**
  Saves API key to disk for later use and offline access.
  @warning Call this method before any other API request to avoid 401 (Unauthorized) response.
