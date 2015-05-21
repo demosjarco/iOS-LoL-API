428707760b6713be8422c0bbf6d6c831e04bcc2d
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2015-05-21 10:56:31 -0700

    Added detail views

---
+++ LoL API Explorer/LoL API Explorer/EndpointBrowserDetailViews/DetailDictionary.h
@@ -0,0 +1,13 @@
+//
+//  DetailDictionary.h
+//  LoL API Explorer
+//
+//  Created by Victor Ilisei on 5/21/15.
+//  Copyright (c) 2015 Tech Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailDictionary : UITableViewController
+
+@end
---
+++ LoL API Explorer/LoL API Explorer/EndpointBrowserDetailViews/DetailEverything.h
@@ -0,0 +1,13 @@
+//
+//  DetailEverything.h
+//  LoL API Explorer
+//
+//  Created by Victor Ilisei on 5/21/15.
+//  Copyright (c) 2015 Tech Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailEverything : UITableViewController
+
+@end
