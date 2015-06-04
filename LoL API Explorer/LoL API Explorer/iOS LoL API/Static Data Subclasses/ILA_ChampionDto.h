b4b8cfc66827edb1b0c5dec6d839eb4a2869122e
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2015-06-03 18:19:06 -0700

    Added static data champion info subclass

---
+++ LoL API Explorer/LoL API Explorer/iOS LoL API/Static Data Subclasses/ILA_ChampionDto.h
@@ -0,0 +1,13 @@
+//
+//  ILA_ChampionDto.h
+//  LoL API Explorer
+//
+//  Created by Victor Ilisei on 6/3/15.
+//  Copyright (c) 2015 Tech Genius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ILA_ChampionDto : NSObject
+
+@end
